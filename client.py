116f249203e2c892f1207b8502b1a83c95feb72c
KimRuedasR <kruedas09@alumnos.uaq.mx>
2023-05-28 22:38:33 -0600

    🗿 add P2P chat, menu  🗿

--- client.py
+++ client.py
@@ -1,225 +1,77 @@
+# client.py
+import socket
+import threading
 import os
-import time
-from socket import *
-from functionalities import *
-from datetime import datetime
-from threading import Thread
-from os import system
 
-now = datetime.now()
+class Cliente:
+    def __init__(self, host = 'localhost', port = 5000):
+        self.apodo = input("\nIngresa tu apodo: ")
+        self.cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.cliente.connect((host, port))
 
-HEADER = 16
+    def recibir(self):
+        while True:
+            try:
+                mensaje = self.cliente.recv(1024).decode('utf-8')
+                if mensaje == 'Nombre':
+                    self.cliente.send(self.apodo.encode('utf-8'))
+                else:
+                    print(mensaje)
+            except:
+                print("Error")
+                self.cliente.close()
+                break
 
-#main server socket
-host = '148.220.208.138'
-port = 10000
-address = (host, port)
-mainsock = socket(AF_INET, SOCK_STREAM)
+    def escribir(self):
+        while True:
+            print("\n1. Enviar mensaje")
+            print("2. Enviar archivo")
+            print("3. Recibir archivo")
+            print("4. Salir")
+            opcion = input("Elige una opción: ")
 
-#listening channel info
-myhost = '148.220.208.138'
-myport = 10001
-myaddress = (myhost, myport)
-sock = socket(AF_INET, SOCK_STREAM)
+            if opcion == "1":
+                mensaje = f'{self.apodo}: {input("Escribe tu mensaje: ")}'
+                self.cliente.send(mensaje.encode('utf-8'))
 
-#triger
-triger = 0
+            elif opcion == "2":
+                nombre_archivo = input("\nIngresa el nombre del archivo a enviar: ")
+                self.enviar_archivo(nombre_archivo, self.cliente)
 
-#user info
-myusername = ''
+            elif opcion == "3":
+                nombre_archivo = input("\nIngresa el nombre del archivo a recibir: ")
+                self.recibir_archivo(nombre_archivo, self.cliente)
 
-#client control
-socketslist = []                     # contiene los sockets activos para ser leídos
-contacts    = {'server': address}    # {'username' : ('ip', int(port))} 
-activeUsers = {}                     # {sockets : ('ip', int(port))} -----> [idPeer] {sockets : 'username'}
-activeUsers2= {}                     # [idPeer] {'username' : socket}
-groups      = {}                     # {'group name' : ['member1', 'member2', 'member3', ...]}
-keys        = {}                     # {'sockets' : b'public key'}
+            elif opcion == "4":
+                print("\nSaliendo...")
+                self.cliente.close()
+                break
 
-#requests control
-requestSent = []
+            else:
+                print("\nOpción no reconocida. Intenta de nuevo.")
 
-#crypto
-public_key = ''
-private_key = ''
-
-
-
-def listeningChannel():
-    sock.bind(myaddress)
-    sock.listen(1)
-    sock.setblocking(0)
-
-    while True:
-        try:
-            client, clientadd = sock.accept()
-            print(f"::::: {clientadd}> Conexión exitosa")
-
-            socketslist.append(client)
-            activeUsers[client] = clientadd
-        except:
-            pass
-
-        if len(socketslist) > 0:
-            for sockets in socketslist:
-                message = recvmessages(sockets, HEADER)
-                if message != 0:
-                    try:
-                        message = message.decode('utf-8')
-                    except:
-                        message = decryptMessage(private_key, message).decode('utf-8')
-
-                    if message == 'recvKey':
-                        if int(recvKey(sockets, keys, HEADER)):
-                            sendKey(sockets, HEADER, public_key)
-                        print(f'received from{activeUsers[sockets]}: ', keys[sockets])
-                        sockets.setblocking(0)
-
-                    elif message == 'loginResponse':
-                        global myusername, triger
-                        myusername, triger = loginResponse(sockets, keys, private_key, myhost, myport, HEADER)
-                        sockets.setblocking(0)
-
-                    elif message == 'confirmationUserCreation':
-                        triger = userCreationResponse(sockets, private_key, HEADER)
-                        sockets.setblocking(0)
-
-                    elif message == 'requestConfirmation':
-                        recvReqConfirmation(sockets, private_key, requestSent, HEADER)
-                        sockets.setblocking(0)
-
-                    elif message == 'recvRequest':
-                        chatRequest(sockets, keys, private_key, HEADER)
-                        sockets.setblocking(0)
-
-                    elif message == 'recvRequestResponse':
-                        if recvRequestResponse(sockets, private_key, requestSent, HEADER):
-                            addContact(sockets, private_key, contacts, HEADER)
-                        sockets.setblocking(0)
-
-                    elif message == 'addContact':
-                        addContact(sockets, private_key, contacts, HEADER)
-                        sockets.setblocking(0)
-
-                    elif message == 'joinedGroup':
-                        recvGroupInfo(sockets, private_key, groups, contacts, activeUsers, myusername, HEADER)
-                        sockets.setblocking(0)
-
-                    elif message == 'groupMessage':
-                        recvGroupMessage(sockets, activeUsers, private_key, HEADER)
-                        sockets.setblocking(0)
-                    
-                    elif message == 'groupFile':
-                        recvGroupFile(sockets, activeUsers, private_key, HEADER)
-                        sockets(0)
-
-                    elif message == 'idPeer':
-                        idPeer(sockets, private_key, activeUsers, activeUsers2, HEADER)
-                        sockets.setblocking(0)
-
-                    elif message == 'recvFile':
-                        recvFile(sockets, private_key, activeUsers, HEADER)
-                        sockets.setblocking(0)
-
-                    else:
-                        date = now.strftime('%a %d %b, %Y')
-                        time = now.strftime('%H:%M:%S')
-                        print(generateColorText(f'[{date}]', bcolors.OKBLUE), end='')
-                        print(generateColorText(f'[{time}]', bcolors.OKBLUE), end='')
-                        print(generateColorText(f' {activeUsers[sockets]}>', bcolors.WARNING), end='')
-                        print(f" {message}")
-
-
-def mainChannel():
-    global triger
-    mainsock = initializeChat('server', public_key, activeUsers, activeUsers2, socketslist, contacts, HEADER)
-    system("cls")
-    while True:
-        print(generateColorText(f'    [0] login\n    [1] create user', bcolors.HEADER))
-        message = input(generateColorText(f'{myusername}> ', bcolors.HEADER))
-        #login
-        if message == '0':
-            wait = login(mainsock, keys, HEADER)
-            system("cls")
-            if wait:
-                #espera la respuesta del servidor
-                while True:
-                    print(generateColorText('#', bcolors.HEADER), end='')
-                    time.sleep(0.02)
-                    if triger:
-                        triger = 0
-                        break
-                #comprueba si el login fue exitoso o no
-                #para pasar a la siguiente pantalla
-                if myusername != '':
+    def enviar_archivo(self, nombre_archivo, socket):
+        with open(nombre_archivo, 'rb') as f:
+            while True:
+                bytes_leidos = f.read(4096)
+                if not bytes_leidos:
                     break
-            
+                socket.sendall(bytes_leidos)
 
-        elif message == '1':
-            wait = createUser(mainsock, keys, HEADER)
-            system("cls")
-            #espera la respuesta del servidor
-            if wait:
-                while True:
-                    time.sleep(0.02)
-                    print(generateColorText('#', bcolors.HEADER), end='')
-                    if triger:
-                        triger = 0
-                        break
+    def recibir_archivo(self, nombre_archivo, socket):
+        with open(nombre_archivo, 'wb') as f:
+            while True:
+                datos = socket.recv(4096)
+                if not datos:
+                    break
+                f.write(datos)
 
-        time.sleep(1)
-        system("cls")
+    def iniciar(self):
+        hilo_recibir = threading.Thread(target=self.recibir)
+        hilo_recibir.start()
 
-    
-    system("cls")
-    for i in range(1, 15):
-        print(generateColorText('#', bcolors.HEADER), end='')
-        time.sleep(0.01)
+        hilo_escribir = threading.Thread(target=self.escribir)
+        hilo_escribir.start()
 
-    print(generateColorText(f"    Bienvenido {myusername}    ###############", bcolors.HEADER))
-    print(generateColorText(f'\n\n  [MENÚ]', bcolors.HEADER))
-    print(generateColorText(f'        # \'\chat\'       ---->   Abrir chat con alguien de tu lista de contactos.', bcolors.HEADER))
-    print(generateColorText(f'        # \'\\request\'   ---->   Envíar una solicitud de chat.', bcolors.HEADER))
-    print(generateColorText(f'        # \'\\group\'     ---->   Crea un grupo con tus amigos.', bcolors.HEADER))
-    print(generateColorText(f'        # \'\\groupChat\' ---->   Abre el chat con un grupo.', bcolors.HEADER))
-    print(generateColorText(f'        # \'\\logout\'    ---->   Cerrar sesión.', bcolors.HEADER))
-
-    print(generateColorText(f'\n\n  [COMANDOS DE CHAT]', bcolors.HEADER))
-    print(generateColorText(f'        # \'\chat\'       ---->   Abrir chat con otro contacto.', bcolors.HEADER))
-    print(generateColorText(f'        # \'\\back\'      ---->   Volver', bcolors.HEADER))
-    print(generateColorText(f'        # \'\\close\'     ---->   Cerrar chat', bcolors.HEADER))
-
-    while True:
-        message = input(generateColorText(f'{myusername}> ', bcolors.OKGREEN))
-
-        if message == r'\request':
-            requestUser(mainsock, keys, contacts, requestSent, HEADER)
-
-        elif message == '\chat':
-            openChat(contacts, activeUsers, activeUsers2, socketslist, public_key, keys, myusername, HEADER)
-
-        elif message == '\group':
-            createGroup(myusername, contacts, groups, socketslist, activeUsers, activeUsers2, public_key, keys, HEADER)
-                    
-        elif message == '\groupChat':
-            openChatGroup(groups, myusername, socketslist, contacts, activeUsers, activeUsers2, public_key, keys, HEADER)
-
-        elif message == '\logout':
-            logout(socketslist, activeUsers, activeUsers2, keys, HEADER)
-
-        else:
-            sendencryptedmessage(mainsock, keys, message, HEADER)
-
-if __name__ == '__main__':
-
-    private_key, public_key = generateKeys()
-    #print ('my key:', public_key)
-
-    t1 = Thread(target=listeningChannel)
-    t2 = Thread(target=mainChannel)
-
-    t1.start()
-    t2.start()
-
-    t2.join()  # interpreter will wait until your process get completed or terminated
-    os._exit(0)
+cliente = Cliente()
+cliente.iniciar()
