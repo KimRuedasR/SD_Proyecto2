b52a71e572b54b2e980a26826e6fe67696b84437
KimRuedasR <kruedas09@alumnos.uaq.mx>
2023-06-18 21:50:02 -0600

    🗿 refactor(files): file transfer based on last proyect 🗿

--- client.py
+++ client.py
@@ -1,13 +1,12 @@
 import socket
 import threading
-import os
 
 INICIO_TRANSFERENCIA = 'INICIO_TRANSFERENCIA'
 FIN_TRANSFERENCIA = 'FIN_TRANSFERENCIA'
 
 class Cliente:
     # Constructor del Cliente
-    def __init__(self, host = 'localhost', puerto = 6000):
+    def __init__(self, host='localhost', puerto=5000):
         self.apodo = input("Ingresa tu apodo: ")
         self.cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.cliente.connect((host, puerto))
@@ -15,38 +14,24 @@ class Cliente:
 
     # Método para recibir datos del servidor
     def recibir(self):
-        recibiendo_archivo = False
-        datos_archivo = b''
         while True:
-            try:
-                # Recepción del mensaje del servidor
-                mensaje = self.cliente.recv(1024)
-                # Verificar el inicio y final de la transferencia
-                if mensaje.decode('utf-8') == INICIO_TRANSFERENCIA:
-                    recibiendo_archivo = True
-                    datos_archivo = b''
-                    continue
-                elif mensaje.decode('utf-8') == FIN_TRANSFERENCIA:
-                    recibiendo_archivo = False
-                    # Escribir los datos en un archivo
-                    with open('archivo_recibido', 'wb') as f:
-                        f.write(datos_archivo)
-                    continue
-                # Si se está recibiendo un archivo, agregar los datos
-                elif recibiendo_archivo:
-                    datos_archivo += mensaje
-                    continue
-                elif mensaje == 'Apodo':
-                    print('chingadamadre')
-                else:
-                    # Imprimir el mensaje
-                    print(mensaje.decode('utf-8'))
-            except:
-                # Si hay un error, cerrar la conexión
-                print("¡Ocurrió un error!")
-                self.cliente.close()
-                break
-
+            mensaje = self.cliente.recv(1024)
+            # Comprobar si el mensaje es el indicador de inicio de transferencia de archivo
+            if mensaje.decode('utf-8') == INICIO_TRANSFERENCIA:
+                # Crear y abrir un nuevo archivo y escribir los datos recibidos en él
+                with open('archivo_recibido', 'wb') as f:
+                    while True:
+                        data = self.cliente.recv(1024)
+                        # Comprobar si los datos recibidos son el indicador de fin de transferencia
+                        if data.endswith(FIN_TRANSFERENCIA.encode('utf-8')):
+                            # Escribir los datos restantes y terminar el bucle
+                            f.write(data[:-len(FIN_TRANSFERENCIA)])
+                            break
+                        # Escribir los datos en el archivo
+                        f.write(data)
+            else:
+                # Si el mensaje no es un indicador de transferencia, imprimir el mensaje
+                print(mensaje.decode('utf-8'))
 
     # Método para enviar datos al servidor
     def escribir(self):
@@ -54,7 +39,6 @@ class Cliente:
             print("\nElige una opción: ")
             print("1. Chat")
             print("2. Enviar archivo")
-            print("3. Recibir archivo")
             opcion = input()
             if opcion == "1":
                 while True:
@@ -65,32 +49,19 @@ class Cliente:
                     self.cliente.send(mensaje_completo.encode('utf-8'))
             elif opcion == "2":
                 nombre_archivo = input("\n--Ingresa el nombre del archivo a enviar: ")
+                # Enviar el indicador de inicio de transferencia al servidor
                 self.cliente.send(INICIO_TRANSFERENCIA.encode('utf-8'))
-                self.enviar_archivo(nombre_archivo, self.cliente)
+                # Leer el archivo y enviar los datos al servidor
+                with open(nombre_archivo, 'rb') as f:
+                    while True:
+                        data = f.read(1024)
+                        if not data:
+                            break
+                        self.cliente.send(data)
+                # Enviar el indicador de fin de transferencia al servidor
                 self.cliente.send(FIN_TRANSFERENCIA.encode('utf-8'))
-            elif opcion == "3":
-                nombre_archivo = input("\n--Ingresa el nombre del archivo a recibir: ")
-                self.recibir_archivo(nombre_archivo, self.cliente)
             else:
                 print("\nOpción incorrecta. Intenta de nuevo.")
-
-    # Método para enviar un archivo al servidor
-    def enviar_archivo(self, nombre_archivo, socket):
-        with open(nombre_archivo, 'rb') as f:
-            while True:
-                bytes_leidos = f.read(4096)
-                if not bytes_leidos:
-                    break
-                socket.sendall(bytes_leidos)
-
-    # Método para recibir un archivo del servidor
-    def recibir_archivo(self, nombre_archivo, socket):
-        with open(nombre_archivo, 'wb') as f:
-            while True:
-                datos = socket.recv(4096)
-                if not datos:
-                    break
-                f.write(datos)
 
     # Método para iniciar los threads de recibir y escribir
     def iniciar(self):
@@ -100,6 +71,6 @@ class Cliente:
         hilo_escribir = threading.Thread(target=self.escribir)
         hilo_escribir.start()
 
-# Creación y inicio del cliente
+# Creación e inicio del cliente
 cliente = Cliente()
 cliente.iniciar()
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ FIN_TRANSFERENCIA = 'FIN_TRANSFERENCIA'
 
 class Servidor:
     # Constructor del servidor
-    def __init__(self, host = 'localhost', puerto = 6000):
+    def __init__(self, host = 'localhost', puerto = 5000):
         self.host = host
         self.puerto = puerto
         # Inicialización del socket
@@ -17,68 +17,32 @@ class Servidor:
     # Método para enviar un mensaje a todos los clientes
     def difundir(self, mensaje, cliente):
         for c in self.clientes:
-            # No enviar el mensaje al cliente emisor
-             if c != cliente:
-                try:
-                    c.send(mensaje)
-                except BrokenPipeError:
-                    # Manejo de errores
-                    print(f"No se pudo enviar mensaje a {self.clientes[c]}. Cliente desconectado.")
-                    c.close()
-                    del self.clientes[c]
-                    
-    # Método para transferencia de datos
+            if c != cliente:  # No enviar mensaje al remitente
+                c.send(mensaje)
+
+    # Método para manejar las transferencias de datos
     def manejar(self, cliente):
-        try:
-            apodo = self.clientes[cliente]
-        except KeyError:
-            print("Cliente desconocido.")
-            return
-        recibiendo_archivo = False
-        datos_archivo = b''
         while True:
             try:
-                # Recepción del mensaje del cliente
                 mensaje = cliente.recv(1024)
-                # Verificar el inicio y final de la transferencia
-                if mensaje.decode('utf-8') == INICIO_TRANSFERENCIA:
-                    recibiendo_archivo = True
-                    datos_archivo = b''
-                    continue
-                elif mensaje.decode('utf-8') == FIN_TRANSFERENCIA:
-                    recibiendo_archivo = False
-                    continue
-                # Si se está recibiendo un archivo, agregar los datos
-                elif recibiendo_archivo:
-                    datos_archivo += mensaje
-                    continue
-                # Enviar el mensaje a todos los demás clientes
                 self.difundir(mensaje, cliente)
             except:
-                # Si hay un error, eliminar el cliente de la lista de clientes
+                apodo = self.clientes[cliente]  # Guardar el apodo antes de eliminar al cliente
                 del self.clientes[cliente]
-                # Cerrar la conexión con el cliente
                 cliente.close()
-                # Notificar a todos los clientes que este cliente ha dejado el chat
                 self.difundir(f"\n**{apodo} dejó el chat".encode('utf-8'), cliente=None)
                 break
 
     # Método para aceptar nuevas conexiones
     def recibir(self):
         while True:
-            # Aceptar una nueva conexión
             cliente, direccion = self.servidor.accept()
             print(f"Conectado con {str(direccion)}")
-            
-            # Solicitar y recibir el apodo del cliente
             apodo = cliente.recv(1024).decode('utf-8')
             self.clientes[cliente] = apodo
-            
-            print(f"El apodo del cliente es {apodo}")
+            print(f"El apodo del cliente es {apodo}")  # Imprimir el apodo del cliente
             self.difundir(f'\n+¡@{apodo} se unió al chat!'.encode('utf-8'), cliente)
             cliente.send('*** ¡Conectado al servidor! ***'.encode('utf-8'))
-
-            #Iniciar un nuevo hilo para manejar la comunicación con el cliente
             hilo = threading.Thread(target=self.manejar, args=(cliente,))
             hilo.start()
 
